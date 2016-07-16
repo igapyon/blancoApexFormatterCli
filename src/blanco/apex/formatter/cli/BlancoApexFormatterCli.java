40355b0f624ee1f3809b6db76fbbba8d3593f0b6
Toshiki Iga <igapyon@gmail.com>
2016-07-16 21:04:04 +0900

    adding cli interface.

--- src/blanco/apex/formatter/cli/BlancoApexFormatterCli.java
+++ src/blanco/apex/formatter/cli/BlancoApexFormatterCli.java
@@ -15,6 +15,14 @@
  */
 package blanco.apex.formatter.cli;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import blanco.apex.formatter.BlancoApexFormatterConstants;
 import blanco.apex.parser.BlancoApexConstants;
 import blanco.apex.syntaxparser.BlancoApexSyntaxConstants;
@@ -24,5 +32,33 @@ public class BlancoApexFormatterCli {
 		System.err.println("blancoApexFormatter: " + BlancoApexFormatterConstants.getVersion());
 		System.err.println("     lexical parser: " + BlancoApexConstants.getVersion());
 		System.err.println("      syntax parser: " + BlancoApexSyntaxConstants.getVersion());
+
+		final Options options = new Options();
+		options.addOption(Option.builder("i").required(true).hasArg(true) //
+				.argName("inputdir") //
+				.desc("input directory.").build());
+		options.addOption(Option.builder("o").required(true).hasArg(true) //
+				.argName("outputdir") //
+				.desc("output directory.").build());
+		options.addOption(Option.builder("h").required(false).hasArg(false) //
+				.argName("help") //
+				.desc("show usage.").build());
+
+		final CommandLineParser parser = new DefaultParser();
+		try {
+			final CommandLine cmd = parser.parse(options, args);
+
+			if (cmd.hasOption("h")) {
+				showUsage(options);
+			}
+		} catch (ParseException ex) {
+			System.err.println("Parse argument failed. Reason: " + ex.getMessage());
+			showUsage(options);
+		}
+	}
+
+	public static void showUsage(final Options options) {
+		final HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp("blancoApexFormatterCli", options);
 	}
 }
