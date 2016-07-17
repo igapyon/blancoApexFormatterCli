54a91f1c43e270b8125eadd7fc7340a6cc42a2b1
Toshiki Iga <igapyon@gmail.com>
2016-07-17 22:24:28 +0900

    update jars.

--- src/blanco/apex/formatter/cli/BlancoApexFormatterCliConstants.java
+++ src/blanco/apex/formatter/cli/BlancoApexFormatterCliConstants.java
@@ -1,7 +1,7 @@
 package blanco.apex.formatter.cli;
 
 public class BlancoApexFormatterCliConstants {
-	public static final String VERSION = "v0.7.2-20160717d";
+	public static final String VERSION = "v0.7.3-20160717a";
 
 	public static String getVersion() {
 		return VERSION;
