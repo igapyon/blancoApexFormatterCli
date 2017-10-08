9179100628c1f5e03605cf9d1e07af7d3096f54c
Toshiki Iga <igapyon@gmail.com>
2017-10-08 16:44:05 +0900

    adding javadoc, set version.

--- src/main/java/blanco/apex/formatter/cli/BlancoApexFormatterCliConstants.java
+++ src/main/java/blanco/apex/formatter/cli/BlancoApexFormatterCliConstants.java
@@ -1,8 +1,21 @@
 package blanco.apex.formatter.cli;
 
+/**
+ * Constants of blancoApexFormatterCli.
+ * 
+ * @author Toshiki Iga
+ */
 public class BlancoApexFormatterCliConstants {
-    public static final String VERSION = "v0.7.3-20160717a";
+    /**
+     * Version String of blancoApexFormatterCli. ex. v1.0.
+     */
+    public static final String VERSION = "v1.0";
 
+    /**
+     * Get version String of blancoApexFormatterCli.
+     * 
+     * @return Version string.
+     */
     public static String getVersion() {
         return VERSION;
     }
